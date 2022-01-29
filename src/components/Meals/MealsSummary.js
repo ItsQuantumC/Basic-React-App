e35f883f902c17fd7298618e464349082cbd15ee
omkar <omkar@Iriss-MacBook-Air.local>
2022-01-29 22:11:34 +0530

    First commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Enjoy Our Delicious Persian Kebabs</h2>
+      <p>
+      Kebab is a cooked meat dish, with its origins in Middle Eastern cuisines. Many variants are popular around the world.In most English-speaking countries, a kebab may be the classic shish kebab or souvlaki – small cubes of meat cooked on a skewer or, outside of North America where it is better known as the more recent and now-ubiquitous doner kebab.
+      </p>
+      <p>
+      Kebabs consist of cut up or ground meat, sometimes with vegetables, and various other accompaniments according to the specific recipe. Although kebabs are typically cooked on a skewer over a fire, some kebab dishes are baked in a pan in an oven or prepared as a stew such as tas kebab.The traditional meat for kebabs is most often mutton or lamb, but regional recipes may include beef, goat, chicken, fish, or more rarely due to religious prohibitions, pork.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
